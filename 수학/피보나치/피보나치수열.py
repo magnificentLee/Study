13f1ee0366759ada977f11e05d38ce392d6936cd
jeoing lee <leer9999@gmail.com>
2022-04-08 23:49:31 +0900

    daily routine

--- 수학/피보나치/피보나치수열.py
+++ 수학/피보나치/피보나치수열.py
@@ -1,11 +1,11 @@
 # 1, 1, 2, 3, 5, 8, ...
 
-def 피보나치(n):
+def fibo(n):
     if n <= 1:
         return n
     else:
-        return 피보나치(n - 1) + 피보나치(n - 2)
+        return fibo(n - 1) + fibo(n - 2)
 
 
 for i in range(1, 11):
-    print(피보나치(i))
+    print(fibo(i))
